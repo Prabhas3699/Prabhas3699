14b393af3e2c6f0c1c442a59028a4c07a9d32941
Prabhas3699 <prabhak620@gmail.com>
2022-06-21 09:33:03 +0530

    add java file

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+          public static void main(string[] args) {
+          while(true)
+          {
+                  system.out.println("Hello World!!!");
+                  try {
+                            Thread.sleep(5000);
+                  }  catch(exception e) {
+                  }
+           }
+      }
+}
+
