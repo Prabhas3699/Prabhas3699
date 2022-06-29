6cbea21394b6e43a0b3cf4cd754ad70aebafd93c
Prabhas3699 <prabhak620@gmail.com>
2022-06-29 11:45:23 +0530

    java program

---
+++ HarmonicSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HarmonicSeries {
+
+	public static void main(String[] args) {
+		
+		 int num=0;
+		 int i = 1;
+	     double rst = 0.0;
+
+	        Scanner in = new Scanner(System.in);
+	        System.out.println("Enter the number for length of series");
+	        num = in.nextInt();
+
+	        while (i <= num) {
+	            System.out.print("1/" + i + " +");
+	            rst = rst + (double) 1 / i;
+	            i++;
+	        }System.out.println("\n\nSum of Harmonic Series is " + rst);
+	}
+}
