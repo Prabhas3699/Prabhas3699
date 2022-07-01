9beeb251ccbc03257a71326ec7429c84273ed2b7
Prabhas3699 <prabhak620@gmail.com>
2022-07-01 17:10:03 +0530

    java file

---
+++ LineComparisonProblem.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class LineComparisonProblem {
+
+	public static void main(String[] args) {
+		 System.out.println("Welcome Line Comparision Computation Program");
+
+	        double x1 = 5.5;
+	        double x2 = 6.0;
+	        double y1 = 7.0;
+	        double y2 = 8.5;
+	        double lengthOfLine = Math.sqrt((Math.pow((x2-x1), 2) + (Math.pow((y2-y1), 2))));
+	        System.out.println("Length of the line: " +lengthOfLine);
+
+	}
+
+}
