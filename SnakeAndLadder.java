e9bef48e0d6d57091916b641716cfef8e3103c3e
Prabhas3699 <prabhak620@gmail.com>
2022-07-03 13:07:56 +0530

    java file

---
+++ SnakeAndLadder.java
@@ -0,0 +1,13 @@
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		int position=0;
+		
+		System.out.println("The position of the player start at"+position);
+		
+
+	}
+
+}
